a1497e7c2e0555f585634a1bf87186ecf50343e3
Baochuan Lu <lubaochuan@gmail.com>
2015-09-30 17:17:16 +0000

    Add require and private methods to games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  require 'hangperson_game'
+
+  private
+    def load_game
+      @game = YAML.load(cookies[:game])
+    end
+    
+    def store_game
+      cookies[:game] = @game.to_yaml
+    end
 end
