9df35d5176bc3ed994a29d43cebcc8c03edcd85f
Spencer Smith <spencer.smith.t@gmail.com>
2015-10-10 02:06:00 +0000

    Raisin-Bagel Hangperson App

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,47 @@
 class GamesController < ApplicationController
   require 'hangperson_game'
 
+  def create
+     @game = HangpersonGame.new HangpersonGame.get_random_word
+     store_game
+     redirect_to '/show'
+  end
+  
+  def show
+    load_game
+    case @game.check_win_or_lose
+    when :win
+      redirect_to '/win'
+    when :lose
+      redirect_to '/lose'
+    end
+  end
+  
+  def guess
+    load_game
+    letter = params[:guess].to_s || ' '  
+    if @game.guesses.include?(letter) or 
+       @game.wrong_guesses.include?(letter)
+      flash[:alert] = "You have already used that letter"
+    end
+   
+    @game.guess letter
+    store_game
+    redirect_to '/show'
+  end
+
+  
+  def win
+    load_game
+  end
+
+  def lose
+    load_game
+  end
+
+  
+  
+  
   private
     def load_game
       @game = YAML.load(cookies[:game])
